b83144f4373c7c1b449ac6b467b019f982a72b7b
Samantha-Ramirez <samantha.p.ramirez.y@gmail.com>
2025-05-24 19:55:11 -0400

    chore: Validaciones de negativos

--- Proyecto1.hs
+++ Proyecto1.hs
@@ -2,9 +2,29 @@
     Colaboradores: 
     - Marcel Mejias 30514210 
     - Samantha Ramirez 31307714
+--}
+{-- 
     Diseñar un programa que permita determinar cuántos litros de cerveza deben agregarse entre los barriles para servir 
     exactamente n vasos de cerveza desde cualquiera de las salidas
+    Vaso
+    - 1 vaso = 1L cerveza
+    - Se sirve desde cualquier barril que cumpla con la cantidad solicitada
+    Barril
+    - Diferentes capacidades
+    - Tienen una salida para servir en el vaso
+    - Transferencias entre ellos respetan sus capacidades máximas
+    - Posibles transferencias A <-> B <-> C 
+    - Posibles agregaciones desde A, C
 --}
+{--
+    Funciones adicionales
+--}
+-- Devolver 0 si el número es negativo
+notNegative :: Int -> Int
+notNegative x = max x 0
+
+validatedBarrel :: Barrel -> Barrel
+validatedBarrel (maxCapacity, currentAmount) = (notNegative maxCapacity, notNegative currentAmount)
 
 {-- 
     Parte 1: Inicialización de barriles 
@@ -13,9 +33,8 @@
 --}
 type Barrel = (Int, Int) -- (capacidad máxima, cantidad actual)
 
-
 initialBarrels :: Barrel -> Barrel -> Barrel -> (Barrel, Barrel, Barrel)
-initialBarrels a b c = (a, b, c)
+initialBarrels a b c = (validatedBarrel a, validatedBarrel b, validatedBarrel c)
 
 {-- 
     Parte 2: Existe solución 
